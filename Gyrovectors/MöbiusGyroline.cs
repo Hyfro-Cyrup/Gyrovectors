4d4f08cd6ccbdf3ac95e5e1d3de942eef39ac15c
Hyfro_Cyrup <skaternathan243@gmail.com>
2025-07-07 20:36:07 -0500

    Create MobiusGyroline class

---
+++ Gyrovectors/MöbiusGyroline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyrovectors;
+
+public class MöbiusGyroline
+{
+    public readonly MöbiusGyrovector a, b;
+    public readonly Complex center;
+    public readonly double radius;
+
+    public MöbiusGyroline(MöbiusGyrovector a, MöbiusGyrovector b) {
+        this.a = a;
+        this.b = b;
+        double A = (MöbiusGyrovector.NormSquared(a) - MöbiusGyrovector.NormSquared(b)) / (2 * (a.x - b.x));
+        double B = (b.y - a.y) / (a.x - b.x);
+        double center_y = (MöbiusGyrovector.NormSquared(a) - 2 * A * a.x + 1) / (2 * (B * a.x + a.y));
+        double center_x = A + B * center_y;
+
+        center = new Complex(center_x, center_y);
+        radius = (new Complex(center_x - a.x, center_y - a.y)).Magnitude;
+    }
+
+}
